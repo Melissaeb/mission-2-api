3a2a265a449c11bd23ca1ef38a6d4e0ac59f10f1
Melissa Elliston-Boyes <m.ellistonboyes@gmail.com>
2024-05-29 11:54:49 +1200

    Refactored API 1 and test

--- controllers/carValueController.js
+++ controllers/carValueController.js
@@ -6,6 +6,7 @@ exports.calculateCarValue = (req, res) => {
   const earliestYear = 1885;
   const regex = /^\W+$/;
 
+  // Error handling:
   switch (true) {
     case model === "" || year === "":
       res.status(400).json({ error: "One or more values are missing" });
@@ -27,58 +28,9 @@ exports.calculateCarValue = (req, res) => {
           "Year is outside of the accepted range. Please input a year between 1885 and the current year",
       });
       break;
-    default:
-      const carValue = carValueModel.calculateValue(model, year);
-      res.json({ value: carValue });
   }
+
+  // Output for correct inputs:
+  const carValue = carValueModel.calculateValue(model, year);
+  res.json({ value: carValue });
 };
-
-// const express = require("express");
-// const app = express();
-// app.use(express.json());
-
-// const getCarValue = (req, res) => {
-//   const { model, year } = req.body;
-//   const currentYear = new Date().getFullYear();
-//   const earliestYear = 1885;
-//   const regex = /^\W+$/;
-
-//   switch (true) {
-//     case model === "" || year === "":
-//       res.status(400).json({ error: "One or more values are missing" });
-//       break;
-//     case typeof year !== "number":
-//       res.status(400).json({
-//         error: "Year not recognised. Please use a valid year written in digits",
-//       });
-//       break;
-//     case regex.test(model):
-//       // Tests if the model does not contain numbers or letters
-//       res
-//         .status(400)
-//         .json({ error: "Model not recognised. Please input a valid model" });
-//       break;
-//     case year < earliestYear || year > currentYear:
-//       res.status(400).json({
-//         error:
-//           "Year is outside of the accepted range. Please input a year between 1885 and the current year",
-//       });
-//       break;
-//     default:
-//       // Finds value for each character (a=1, b=1, c=3 etc.)
-//       const characterValue = (letter) =>
-//         letter.toLowerCase().charCodeAt(0) - 96;
-
-//       // Splits the model into separate characters and adds all characterValues, filtering out non-alphabetical characters
-//       const modelValue = model.split("").reduce((sum, letter) => {
-//         const letterValue = characterValue(letter);
-//         return letterValue >= 1 && letterValue <= 26 ? sum + letterValue : sum;
-//       }, 0);
-
-//       const carValue = modelValue * 100 + year;
-
-//       res.json({ value: carValue });
-//   }
-// };
-
-// module.exports = { getCarValue };
--- tests/carValue.test.js
+++ tests/carValue.test.js
@@ -2,7 +2,9 @@ const request = require("supertest");
 const expressApp = require("../app");
 
 describe("POST /api/car-value", () => {
-  it("gives correct response for valid inputs", async () => {
+  const currentYear = new Date().getFullYear();
+
+  it("Test 1: gives correct response for valid inputs", async () => {
     const response = await request(expressApp)
       .post("/api/car-value")
       .send({ model: "Corolla", year: 2015 });
@@ -11,41 +13,16 @@ describe("POST /api/car-value", () => {
     expect(response.body).toEqual({ value: 9615 });
   });
 
-  const test2Cases = [
-    { model: "axela", year: 2009, expectedValue: 6309 },
-    { model: "COROLLA", year: 2015, expectedValue: 9615 },
-    { model: "aXeLa", year: 2009, expectedValue: 6309 },
-  ];
+  it(`Test 2: gives correct response when model input included spaces, capitals, numbers and/or other symbols`, async () => {
+    const response = await request(expressApp)
+      .post("/api/car-value")
+      .send({ model: "Mazda RX-8", year: 2006 });
 
-  test2Cases.forEach(({ model, year, expectedValue }) => {
-    it(`is case-insensitive for model inputs (${model})`, async () => {
-      const response = await request(expressApp)
-        .post("/api/car-value")
-        .send({ model, year });
-
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual({ value: expectedValue });
-    });
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ value: 10706 });
   });
 
-  const test3Cases = [
-    { model: "Ford Mustang GT", year: 2018, expectedValue: 18518 },
-    { model: "RX7", year: 2023, expectedValue: 6223 },
-    { model: "RX-8", year: 2006, expectedValue: 6206 },
-  ];
-
-  test3Cases.forEach(({ model, year, expectedValue }) => {
-    it(`gives correct response when model input included spaces, numbers and/or other symbols (${model})`, async () => {
-      const response = await request(expressApp)
-        .post("/api/car-value")
-        .send({ model, year });
-
-      expect(response.status).toBe(200);
-      expect(response.body).toEqual({ value: expectedValue });
-    });
-  });
-
-  it("gives correct response when model input only includes numbers", async () => {
+  it("Test 3: gives correct response when model input only includes numbers", async () => {
     const response = await request(expressApp)
       .post("/api/car-value")
       .send({ model: "216", year: 2023 });
@@ -54,13 +31,13 @@ describe("POST /api/car-value", () => {
     expect(response.body).toEqual({ value: 2023 });
   });
 
-  const test5Cases = [
+  const test4Cases = [
     { model: "Accord", year: 1885, expectedValue: 6285 },
-    { model: "Accord", year: 2024, expectedValue: 6424 },
+    { model: "Accord", year: currentYear, expectedValue: 6424 },
   ];
 
-  test5Cases.forEach(({ model, year, expectedValue }) => {
-    it(`gives correct response when year is just inside accepted range (${year})`, async () => {
+  test4Cases.forEach(({ model, year, expectedValue }) => {
+    it(`Test 4: gives correct response when year is just inside accepted range (1885 - ${currentYear}) (test: ${year})`, async () => {
       const response = await request(expressApp)
         .post("/api/car-value")
         .send({ model, year });
@@ -70,15 +47,14 @@ describe("POST /api/car-value", () => {
     });
   });
 
-  const test6Cases = [
+  const test5Cases = [
     { model: "Corolla", year: 1884 },
-    { model: "Corolla", year: 0 },
-    { model: "Corolla", year: 3000 },
+    { model: "Corolla", year: currentYear + 1 },
     { model: "Corolla", year: -2015 },
   ];
 
-  test6Cases.forEach(({ model, year }) => {
-    it(`gives error response when year is outside accepted range (1885 - current year) (test: ${year})`, async () => {
+  test5Cases.forEach(({ model, year }) => {
+    it(`Test 5: gives error response when year is outside accepted range (1885 - ${currentYear}) (test: ${year})`, async () => {
       const response = await request(expressApp)
         .post("/api/car-value")
         .send({ model, year });
@@ -90,7 +66,7 @@ describe("POST /api/car-value", () => {
     });
   });
 
-  it("gives error response when model does not include letters or numbers (@*()$)", async () => {
+  it("Test 6: gives error response when model does not include letters or numbers (@*()$)", async () => {
     const response = await request(expressApp)
       .post("/api/car-value")
       .send({ model: "@*()$", year: 2023 });
@@ -101,14 +77,13 @@ describe("POST /api/car-value", () => {
     });
   });
 
-  const test8Cases = [
+  const test7Cases = [
     { model: "", year: 2006 },
     { model: "Corolla", year: "" },
-    { model: "", year: "" },
   ];
 
-  test8Cases.forEach(({ model, year }) => {
-    it(`gives error response when one or more values are missing`, async () => {
+  test7Cases.forEach(({ model, year }) => {
+    it(`Test 7: gives error response when one or more values are missing (model: ${model}, year: ${year})`, async () => {
       const response = await request(expressApp)
         .post("/api/car-value")
         .send({ model, year });
@@ -120,7 +95,7 @@ describe("POST /api/car-value", () => {
     });
   });
 
-  it("gives error response when year is not written with digits", async () => {
+  it("Test 8: gives error response when year is not written with digits", async () => {
     const response = await request(expressApp)
       .post("/api/car-value")
       .send({ model: "Corolla", year: "twenty twenty" });
